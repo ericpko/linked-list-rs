47a4d03112fbbac413c4081aef969c53b621fbd2
Eric K <eric.koehli@mail.utoronto.ca>
2022-11-20 12:22:42 -0500

    finished append method and implemented Display

--- src/linked_list.rs
+++ src/linked_list.rs
@@ -1,6 +1,6 @@
-use std::rc::Rc;
+use std::{cell::RefCell, fmt, rc::Rc};
 
-type Link<T> = Option<Rc<Node<T>>>;
+type Link<T> = Option<Rc<RefCell<Node<T>>>>;
 
 pub struct Node<T> {
     pub item: T,
@@ -34,13 +34,67 @@ impl<T> LinkedList<T> {
     }
 
     pub fn append(&mut self, item: T) {
+        let node = Rc::new(RefCell::new(Node::new(item, None, None)));
+
         // case 1: empty list
         if self.len == 0 {
-            let node = Rc::new(Node::new(item, None, None));
             self.head = Some(Rc::clone(&node));
             self.tail = Some(Rc::clone(&node));
+
+        // case 2: one node list
+        } else if self.len == 1 {
+            if let Some(tail) = self.tail.clone() {
+                // let mut tail = tail.borrow_mut();
+                // tail.next = Some(Rc::clone(&node));
+                tail.borrow_mut().next = Some(Rc::clone(&node));
+                node.borrow_mut().prev = Some(Rc::clone(&tail));
+            }
+
+        // case 3: at least 2 nodes in list
+        } else {
+            if let Some(tail) = self.tail.clone() {
+                tail.borrow_mut().next = Some(Rc::clone(&node));
+                node.borrow_mut().prev = Some(Rc::clone(&tail));
+            }
         }
 
+        self.tail = Some(Rc::clone(&node));
         self.len += 1;
+    }
+
+    pub fn prepend(&mut self, _item: T) {
+        todo!()
+    }
+}
+
+impl<T> fmt::Display for LinkedList<T>
+where
+    T: fmt::Display,
+{
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let mut curr = self.head.clone();
+        while let Some(curr_ptr) = curr {
+            write!(f, "{} -> ", curr_ptr.borrow().item)?;
+            curr = curr_ptr.borrow().next.clone();
+        }
+
+        writeln!(f, "None")
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_append() {
+        let mut list = LinkedList::new();
+        list.append(1);
+        list.append(2);
+        list.append(3);
+        list.append(4);
+        list.append(5);
+        println!("{}", list);
+        println!("{}", list);
     }
 }
