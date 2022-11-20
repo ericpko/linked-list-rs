31483c7247b614fc33c786004c5e4bbb676506ec
Eric K <eric.koehli@mail.utoronto.ca>
2022-11-19 22:11:47 -0500

    updated lib

--- src/lib.rs
+++ src/lib.rs
@@ -13,12 +13,12 @@ impl<T> Node<T> {
     }
 }
 
-pub struct LinkedList<T> {
+pub struct SinglyLinkedList<T> {
     pub head: Link<T>,
     pub len: u32,
 }
 
-impl<T> LinkedList<T> {
+impl<T> SinglyLinkedList<T> {
     pub fn new(list: Option<Vec<T>>) -> Self {
         let mut head: Link<T> = None;
         let mut len: u32 = 0;
@@ -33,6 +33,37 @@ impl<T> LinkedList<T> {
     }
 }
 
+pub struct LinkedList<T> {
+    pub head: Link<T>,
+    pub tail: Link<T>,
+    len: u32,
+}
+
+impl<T> LinkedList<T> {
+    pub fn new() -> Self {
+        Self {
+            head: None,
+            tail: None,
+            len: 0,
+        }
+    }
+
+    pub fn len(&self) -> u32 {
+        self.len
+    }
+
+    pub fn append(&mut self, item: T) {
+        // case 1: empty list
+        if self.len == 0 {
+            let node = Rc::new(Node::new(item, None));
+            self.head = Some(Rc::clone(&node));
+            self.tail = Some(Rc::clone(&node));
+        }
+
+        self.len += 1;
+    }
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
@@ -40,7 +71,7 @@ mod test {
     #[test]
     fn create_list() {
         let arr = vec![1, 2, 3, 4, 5];
-        let mut list = LinkedList::new(Some(arr.clone()));
+        let mut list = SinglyLinkedList::new(Some(arr.clone()));
         let mut i = 0;
         while let Some(curr) = list.head {
             println!("{}", curr.item);
